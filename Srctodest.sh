a53f3d78d6bd0748b1d17b32a2b28157aec023e5
Techwidot <91528104+Techwidot@users.noreply.github.com>
2023-08-11 23:27:58 +0530

    Update Srctodest.sh

--- Srctodest.sh
+++ Srctodest.sh
@@ -18,3 +18,37 @@ if [ $? -eq 0 ]; then
 else
     echo "Error copying the file"
 fi
+---------------------------------------------------------------------------------------
+
+
+#!/bin/bash
+
+# Check if both source and destination paths are provided
+if [ $# -ne 2 ]; then
+    echo "Usage: $0 <source_file> <destination_path>"
+    exit 1
+fi
+
+source_file="$1"
+destination_path="$2"
+
+# Check if source file exists
+if [ ! -f "$source_file" ]; then
+    echo "Source file '$source_file' not found."
+    exit 2
+fi
+
+# Check if destination path exists, create if not
+if [ ! -d "$destination_path" ]; then
+    echo "Destination path '$destination_path' does not exist. Creating..."
+    mkdir -p "$destination_path"
+fi
+
+# Copy the file to the destination
+cp "$source_file" "$destination_path"
+
+if [ $? -eq 0 ]; then
+    echo "File copied successfully from '$source_file' to '$destination_path'."
+else
+    echo "Error copying file."
+fi
